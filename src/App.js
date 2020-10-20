990610784dfbd4d29bb3c880af9f967c7dd59573
YukiSugaya <sugaya324@gmail.com>
2020-10-19 20:08:55 -0700

    Show realtime messages from firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Input from './Components/Input';
+import Message from './Components/Message';
+import firebase from 'firebase/app';
+import firebaseConfig from './config';
+import 'firebase/auth';
 import './App.css';
+import 'firebase/firestore';
 
-function App() {
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const database = firebase.firestore();
+const App = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    database
+      .collection('messages')
+      .orderBy('createdAt', 'asc')
+      .onSnapshot((snap) => {
+        setMessages([]);
+        snap.forEach((doc) => {
+          const data = doc.data();
+          const message = {
+            id: doc.id || '',
+            message: data.message || '',
+            author: data.author || '',
+            createdAt: (data.createdAt && data.createdAt.seconds * 1000) || '',
+          };
+          console.log(message);
+          setMessages((prev) => [...prev, message]);
+          // setMessages(messages);
+        });
+      });
+  }, []);
+
+  // const handleMessage = (text) => {
+  //   setMessages([...messages, { message: text, author: 'Yuki' }]);
+  // };
+
+  console.log('messages', messages);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">Chat App</header> */}
+      <Input />
+      {messages.map((m, id) => {
+        const obj = {
+          id,
+          message: m.message,
+          author: m.author,
+        };
+        return <Message key={id} {...obj} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
